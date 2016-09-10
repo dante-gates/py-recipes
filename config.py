95da82c1ee91079819c33b833c54926890b73dcb
Dante Gates <dantegates@hotmail.com>
2016-09-10 09:16:49 -0400

    Added non-working examples to doctest

--- config.py
+++ config.py
@@ -3,22 +3,51 @@
 ...     foo = 'foo'
 ...     def afunc(x, y):
 ...         return x + y
+...     alist = [1, 2, 3]
+...     adict = {1: 'one', 2: 'two'}
 ...
+
+Confirming the default values are set normally...
+
 >>> AppConfig.foo
 'foo'
+>>> AppConfig.afunc(1, 2)
+3
+>>> AppConfig.alist
+[1, 2, 3]
+>>> AppConfig.adict
+{1: 'one', 2: 'two'}
+
+We can now alter each attribute...
+
 >>> AppConfig.foo = 'baz'
 >>> AppConfig.foo
 'baz'
+
+>>> AppConfig.afunc = lambda x, y: x - y
 >>> AppConfig.afunc(1, 2)
-3
->>> AppConfig.afunc = lambda x, y: x * y
->>> AppConfig.afunc(1, 2)
-2
+-1
+
+>>> AppConfig.alist.extend([4, 5, 6])
+>>> AppConfig.alist
+[1, 2, 3, 4, 5, 6]
+
+>>> AppConfig.adict[1] = 'zero'
+>>> AppConfig.adict[3] = 'three'
+>>> AppConfig.adict
+{1: 'zero', 2: 'two', 3: 'three'}
+
+Now we can ask our class to restore its default settings...
+
 >>> AppConfig.restore_defaults()
 >>> AppConfig.foo
 'foo'
 >>> AppConfig.afunc(1, 2)
 3
+>>> AppConfig.alist
+[1, 2, 3]
+>>> AppConfig.adict
+{1: 'one', 2: 'two'}
 """
 
 import doctest
